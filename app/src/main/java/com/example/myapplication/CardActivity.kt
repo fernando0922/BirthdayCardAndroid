e8b26b01abff1063eebe209ff89874896550f9b3
fernando0922 <fernando.vidal.uw@gmail.com>
2023-03-02 02:52:34 +0530

    functionallity done, icon related cchanges remain

--- app/src/main/java/com/example/myapplication/CardActivity.kt
+++ app/src/main/java/com/example/myapplication/CardActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import com.google.android.material.snackbar.BaseTransientBottomBar
 import com.google.android.material.snackbar.Snackbar
@@ -17,13 +18,15 @@ class CardActivity : AppCompatActivity() {
         val birthdayMessage:TextView = findViewById(R.id.to_message)
         val fromMessage:TextView = findViewById(R.id.from_message)
 
-        birthdayMessage.text = "${this.getString(R.string.happy_birthday_text)}$otherName!!"
-        fromMessage.text = "${this.getString(R.string.from)}$yourName."
+        birthdayMessage.text = "${this.getString(R.string.happy_birthday_text)} $otherName!!"
+        fromMessage.text = "${this.getString(R.string.from)} $yourName."
 
         Snackbar.make(
             findViewById(R.id.cardCoordinatorLayout),
             R.string.share_information,
-            6000
+            Snackbar.LENGTH_LONG
         ).setBackgroundTint(this.getColor(R.color.back_ground)).show()
+
+
     }
 }
